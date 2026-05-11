60aff2a950dab96170a1a4ec9f39331589606962
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-05-11 21:15:23 +0530

    Time: 0 ms (100%), Space: 43.5 MB (41.75%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] plusOne(int[] arr) {
-        for ( int i = arr.length-1 ; i >= 0 ; i--){
+        for ( int i = arr.length-1 ; i>=0; i--){
             arr[i]++;
-            if(arr[i]<10){
+            if (arr[i] < 10){
                 return arr;
+                
             }
-            arr [i] = 0;
+            arr[i] = 0 ;
         }
-        int result[] = new int[arr.length + 1];
-        result[0] = 1;
+        int [] result = new int [arr.length +1];
+        result [0] = 1;
         return result;
     }
 }
+
+
