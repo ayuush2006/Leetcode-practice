f00f446ae30abc6f3be4e20c473f3702afd465ae
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-06-28 14:08:43 +0530

    Time: 0 ms (100%), Space: 43.3 MB (76.27%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,15 +1,49 @@
+// class Solution {
+//     public void sortColors(int[] arr) {
+//         int n = arr.length ;
+//        int  NoofZeroes = 0  , NoofOnes = 0;
+//        for (int i =0 ; i<arr.length ; i++){
+//         if (arr[i]== 0) NoofZeroes ++;
+//          if (arr[i]== 1)  NoofOnes ++;
+//        }
+//         for (int i =0 ; i<arr.length ; i++){
+//             if(i<NoofZeroes) arr[i] = 0;
+//             else if(i<NoofZeroes + NoofOnes ) arr[i] = 1;
+//             else arr[i] = 2;
+//         }   
+//     }
+// }
 class Solution {
     public void sortColors(int[] arr) {
-        int n = arr.length ;
-       int  NoofZeroes = 0  , NoofOnes = 0;
-       for (int i =0 ; i<arr.length ; i++){
-        if (arr[i]== 0) NoofZeroes ++;
-         if (arr[i]== 1)  NoofOnes ++;
-       }
-        for (int i =0 ; i<arr.length ; i++){
-            if(i<NoofZeroes) arr[i] = 0;
-            else if(i<NoofZeroes + NoofOnes ) arr[i] = 1;
-            else arr[i] = 2;
-        }   
+
+        int n = arr.length;
+        int low = 0, mid = 0, high = n - 1;
+
+        while (mid <= high) {
+
+            if (arr[mid] == 0) {
+                int temp = arr[mid];
+                arr[mid] = arr[low];
+                arr[low] = temp;
+
+                low++;
+                mid++;
+            }
+
+            else if (arr[mid] == 1) {
+                mid++;
+            }
+
+            else {
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+
+                high--;
+            }
+        }
     }
 }
+//public void swap (int[]arr , int low ,  int mid,int  high){
+
+//}
