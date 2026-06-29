295748ffdbb69da738a0205bda9d6b8aaa6b7578
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-06-29 17:29:42 +0530

    Time: 1 ms (9.76%), Space: 48.4 MB (11.13%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+       long power = n;
+       double ans = 1.0;
+// power is neegative 
+    if (power <0){
+    x = 1/x ;
+    power = - power ;
+   }
+   while (power > 0){
+    if (power % 2 == 0){
+        x = x *x ;
+        power = power/2;
+    }
+    else {
+        ans = ans* x;
+        power = power - 1;
+    }
+   }
+    return ans;
+   }
+    }
