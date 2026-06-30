6b12c3880c7af477999597c8c214819021679036
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-06-30 22:38:59 +0530

    Time: 9 ms (37.79%), Space: 55.6 MB (46.19%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int majorityElement(int[] arr) {
+        Arrays.sort(arr);
+
+        int n = arr.length;
+        int count = 1;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (count > n / 2) {
+                return arr[i];
+            }
+        }
+
+        return arr[0];
+    }
+}
