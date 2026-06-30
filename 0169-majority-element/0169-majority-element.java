a68c0af6ce1f5f1b40077aa3f049017f42017dfd
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-06-30 23:19:09 +0530

    Time: 7 ms (44.1%), Space: 55.6 MB (46.19%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,20 +3,22 @@ class Solution {
         Arrays.sort(arr);
 
         int n = arr.length;
-        int count = 1;
+        // int count = 1;
 
-        for (int i = 0; i < n - 1; i++) {
-            if (arr[i] == arr[i + 1]) {
-                count++;
-            } else {
-                count = 1;
-            }
+        // for (int i = 0; i < n - 1; i++) {
+        //     if (arr[i] == arr[i + 1]) {
+        //         count++;
+        //     } else {
+        //         count = 1;
+        //     }
 
-            if (count > n / 2) {
-                return arr[i];
-            }
-        }
+        //     if (count > n / 2) {
+        //         return arr[i];
+        //     }
+        // }
 
-        return arr[0];
+        // return arr[0];
+
+        return arr[n/2];
     }
 }
