52157d66eae3d886e425eea34fe7ea88cac11666
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-07-02 19:36:00 +0530

    Time: 0 ms (100%), Space: 45.4 MB (87.6%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -1,11 +1,11 @@
 class Solution {
-    public String restoreString(String s, int[] brr) {
+    public String restoreString(String s, int[] indices) {
         int n = s.length();
-        char [] arr = new char [n] ;
-        for (int i = 0; i<n ; i++){
-            int a = brr[i] ;
-            arr[a] = s.charAt(i);
-        }
-return new String (arr) ;// arr ko string me convert 
-    }
+       char [] arr = new char [n];
+       for(int i = 0; i<n ; i++){
+        int a = indices[i];
+           arr[a]    = s.charAt(i);
+       }
+       return new  String(arr);
+}
 }
