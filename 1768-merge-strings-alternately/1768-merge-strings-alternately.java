510485bb38dfa77b688c3c19abcd663bf364457d
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-07-04 01:13:29 +0530

    Time: 1 ms (96.01%), Space: 42.8 MB (89.23%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,31 +1,51 @@
+// class Solution {
+//     public String mergeAlternately(String word1, String word2) {
+
+//         String ans = "";
+
+//         int m = word1.length();
+//         int n = word2.length();
+//         int min;
+
+//         if (m < n) {
+//             min = m;
+//         } else {
+//             min = n;
+//         }
+
+//         for (int i = 0; i < min; i++) {
+//             ans += word1.charAt(i);
+//             ans += word2.charAt(i);
+//         }
+
+//         for (int i = min; i < m; i++) {
+//             ans += word1.charAt(i);
+//         }
+
+//         for (int i = min; i < n; i++) {
+//             ans += word2.charAt(i);
+//         }
+
+//         return ans;
+//     }
+// }
+
+
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-
-        String ans = "";
-
-        int m = word1.length();
-        int n = word2.length();
-        int min;
-
-        if (m < n) {
-            min = m;
-        } else {
-            min = n;
+        StringBuilder sb = new StringBuilder();
+        int i=0;
+        int j= 0;
+        while(i<word1.length() || j<word2.length()){
+            if(i<word1.length()){
+                sb.append(word1.charAt(i));
+                i++;
+            }
+            if(j<word2.length()){
+                sb.append(word2.charAt(j));
+                j++;
+            }
         }
-
-        for (int i = 0; i < min; i++) {
-            ans += word1.charAt(i);
-            ans += word2.charAt(i);
-        }
-
-        for (int i = min; i < m; i++) {
-            ans += word1.charAt(i);
-        }
-
-        for (int i = min; i < n; i++) {
-            ans += word2.charAt(i);
-        }
-
-        return ans;
+        return sb.toString();
     }
 }
