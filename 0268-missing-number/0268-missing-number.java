a50b81d6985236f1af52c9961b0d2868bbc013fe
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-07-05 22:51:15 +0530

    Time: 9 ms (7.86%), Space: 48.3 MB (5.64%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -2,25 +2,16 @@ import java.util.Arrays;
 
 class Solution {
     public int missingNumber(int[] arr) {
-
         Arrays.sort(arr);
-
         int n = arr.length;
-
-        // Agar 0 missing hai
         if (arr[0] != 0) {
             return 0;
         }
-
-        // Consecutive numbers check karo
         for (int i = 0; i < n - 1; i++) {
-
             if (arr[i + 1] - arr[i] != 1) {
                 return arr[i] + 1;
             }
         }
-
-        // Agar last number missing hai
         return n;
     }
 }
