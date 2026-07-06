8b424fe18072118e1171fb7b0f5e1bb0ea4a0025
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-07-07 01:47:49 +0530

    Time: 17 ms (80.9%), Space: 49.6 MB (72.39%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -3,7 +3,7 @@ class Solution {
     public int arrayPairSum(int[] arr) {
         Arrays.sort(arr);
         int sum = 0;
-        for (int i = 0; i < arr.length; i += 2) {
+        for (int i = 0; i < arr.length; i += 2) { // i+= 2 isliye (1,2) (3,4) (5,6) = hrr pair ka ist element minimum hoga sort karne ke baad aur aur 1st element me +2 hai isliye
             sum += arr[i];
         }
         return sum;
