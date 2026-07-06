5354598781dc97d72b6ca4bb9ac5dfaf5b539465
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-07-07 03:11:40 +0530

    Time: 15 ms (11.49%), Space: 45.4 MB (88.83%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -6,13 +6,13 @@ class Solution {
         ArrayList<Integer> list = new ArrayList<>();
         int n = digits.length;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {  // hundred place ke liye 
+            for (int j = 0; j < n; j++) { // tenth place ke liye 
+                for (int k = 0; k < n; k++) { // ones place ke liye 
 
-                    if (i != j && j != k && i != k) {
+                    if (i != j && j != k && i != k) { // saari digit different honi chahiye 
 
-                        if (digits[i] != 0 && digits[k] % 2 == 0) {
+                        if (digits[i] != 0 && digits[k] % 2 == 0) { // ist digit 0 nahi aur last digit 2 se sivisible hona chahiye 
 
                             int num = digits[i] * 100 + digits[j] * 10 + digits[k];
 
