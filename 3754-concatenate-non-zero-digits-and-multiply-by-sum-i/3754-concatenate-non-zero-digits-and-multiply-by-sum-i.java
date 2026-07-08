796e4f262a3b70ca60f7c1a173d81e413e2471c6
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-07-08 13:00:53 +0530

    Time: 2 ms (30.05%), Space: 42.9 MB (19.12%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,28 +1,20 @@
 class Solution {
     public long sumAndMultiply(int n) {
-
         StringBuilder sb = new StringBuilder();
         int sum = 0;
-
-        while (n > 0) {
-            int digit = n % 10;
-
-            if (digit != 0) {
+        while (n > 0) { // jb tak no khatam na ho jaye tb tak loop chalega 
+            int digit = n % 10; // last digit nikal rahe hai
+            if (digit != 0) { // last digit 0 na ho tab
                 sb.append(digit);
                 sum += digit;
             }
-
             n /= 10;
         }
-
         sb.reverse();
-
         if (sb.length() == 0) {
             return 0;
         }
-
-        long x = Long.parseLong(sb.toString());
-
+        long x = Long.parseLong(sb.toString()); // sb.toString stringbuilder ko string bana deta hai aur fhir Long.parseLong string ko integer bana deta hai
         return x * sum;
     }
 }
