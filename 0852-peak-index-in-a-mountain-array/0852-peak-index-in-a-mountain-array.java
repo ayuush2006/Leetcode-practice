54e01af067e26844c9678e96a5fe086a12af4901
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-07-12 18:33:58 +0530

    Time: 0 ms (100%), Space: 80.4 MB (40.21%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,11 +1,29 @@
-class Solution {
-    public int peakIndexInMountainArray(int[] arr) {
-        int ans = 0;
-        for (int i = 1; i < arr.length - 1; i++) {
-            if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
-                ans = i;
+         // Linear Search approach
+// class Solution {
+//     public int peakIndexInMountainArray(int[] arr) {
+//         int ans = 0;
+//         for (int i = 1; i < arr.length - 1; i++) {
+//             if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+//                 ans = i;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
+
+            // Binary Search approach
+    
+    class Solution{
+        public int peakIndexInMountainArray(int[] arr) {
+            int n = arr.length;
+            int low = 1 , high = n-2;
+            while (low<=high){
+                int mid = low + (high - low) /2 ;
+                if(arr[mid] > arr[mid-1] && arr[mid]> arr[mid + 1]) return mid ;
+                if(arr[mid] > arr[mid-1] && arr[mid] <arr[mid + 1]) low = mid + 1 ;
+                if(arr[mid] < arr[mid-1] && arr[mid]> arr[mid + 1]) high = mid - 1 ;
             }
+            return -1;
         }
-        return ans;
     }
-}
