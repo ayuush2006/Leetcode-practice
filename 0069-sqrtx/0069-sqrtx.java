7dffd77e02b6704e7afeb23f7342f4de08c41682
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-07-14 01:07:20 +0530

    Time: 1 ms (97.64%), Space: 42.6 MB (61.6%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,20 +1,20 @@
    // easy mathmathical approach
-// class Solution {
-//     public int mySqrt(int x) {
-//        return (int) (Math.sqrt(x));
-//     }
-// } 
+class Solution {
+    public int mySqrt(int x) {
+       return (int) (Math.sqrt(x));
+    }
+} 
 
 //binary search approach
-class Solution {
-   public int mySqrt(int x) {
-    long low = 0 , high = (long)x;
-    while (low<=high){
-        long mid = low + (high - low) /2 ;
-        if(mid*mid ==(long)x) return (int)mid;
-        else if (mid*mid >(long)x) high = mid - 1;
-        else low = mid + 1 ;
-        }
-       return (int)high;
-        }
-    }
+// class Solution {
+//    public int mySqrt(int x) {
+//     long low = 0 , high = (long)x;
+//     while (low<=high){
+//         long mid = low + (high - low) /2 ;
+//         if(mid*mid ==(long)x) return (int)mid;
+//         else if (mid*mid >(long)x) high = mid - 1;
+//         else low = mid + 1 ;
+//         }
+//        return (int)high;
+//         }
+//     }
