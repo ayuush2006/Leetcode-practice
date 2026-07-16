5fa218f412cbb945a69d4f1097844a3b47893928
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-07-16 23:44:06 +0530

    Time: 1 ms (98.73%), Space: 47.6 MB (9.01%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,15 +1,35 @@
-class Solution {
+// class Solution {
+//     public int pivotIndex(int[] arr) {
+//         int n = arr.length;
+//         for (int i =0 ; i<n ; i++){
+//             int leftsum = 0 , rightsum = 0;
+//             for (int j =0 ; j<i ; j++){
+//                 leftsum += arr[j];
+//             }
+//             for (int j = i+1; j<n ; j++){
+//                 rightsum += arr[j];
+//             }
+//             if (leftsum == rightsum) return i;
+//         }
+//         return -1;
+//     }
+// }
+
+    class Solution {
     public int pivotIndex(int[] arr) {
         int n = arr.length;
-        for (int i =0 ; i<n ; i++){
-            int leftsum = 0 , rightsum = 0;
-            for (int j =0 ; j<i ; j++){
-                leftsum += arr[j];
-            }
-            for (int j = i+1; j<n ; j++){
-                rightsum += arr[j];
-            }
-            if (leftsum == rightsum) return i;
+        // Prefix Sum
+        for (int i = 1; i < n; i++) {
+            arr[i] += arr[i - 1];
+        }
+        // Check Pivot
+        for (int i = 0; i < n; i++) {
+            int leftsum = 0;
+            if (i > 0)
+                leftsum = arr[i - 1];
+            int rightsum = arr[n - 1] - arr[i];
+            if (leftsum == rightsum)
+                return i;
         }
         return -1;
     }
