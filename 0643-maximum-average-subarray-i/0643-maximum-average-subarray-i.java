3941e9a2b7c83e352d2a2879a5301a12779523c3
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-07-24 07:42:08 +0530

    Time: 3 ms (74.45%), Space: 69.6 MB (44.61%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -5,18 +5,22 @@ class Solution {
         int low = 0 , high = k - 1;
         int max = Integer.MIN_VALUE;
         int sum = 0 ;
+        // sum of window size k
         for (int i = 0 ; i <= high ; i++ ){
             sum += arr[i] ;
         }
+        // window silde kb tak krenge
         while (high < n){
+            // maximum
             max = Math.max(max, sum);
             low ++ ;
             high ++ ;
             if (high == n){
             break ;
         }
+        // main case laga rahe 
         sum = sum - arr[low - 1] + arr[high];
         }
-        return (double) max / k;
+        return (double) max / k; // average nikal rahe 
     }
 }
