23922d6d8531e3b79df39e22c97ec27bebd258ce
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-07-27 03:03:22 +0530

    Time: 3 ms (74.17%), Space: 69.5 MB (63.23%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -2,7 +2,7 @@
 class Solution {
     public double findMaxAverage(int[] arr, int k) {
         int n = arr.length;
-        int low = 0 , high = k - 1;
+        int low = 0 , high = k - 1; // high = size k - 1 ;
         int max = Integer.MIN_VALUE;
         int sum = 0 ;
         // sum of window size k
