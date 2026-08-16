92ffb1ad7a55d187d1e7caad4ec09ded1187495a
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-08-16 20:57:55 +0530

    Time: 3 ms (99.94%), Space: 202.4 MB (80.65%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -8,23 +8,66 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        if (head == null || head.next == null) {
-            return null;
-        }
-        int size = 0;
-        ListNode temp = head;
-        while (temp != null) {
-            size++;
-            temp = temp.next;
-        }
-        int mid = size / 2;
-        temp = head;
-        for (int i = 1; i < mid; i++) {
-            temp = temp.next;
-        }
-        temp.next = temp.next.next;
-        return head;
+     ListNode slow = head;
+     ListNode fast = head;
+     if(head.next == null) return null;
+     while(fast.next.next!= null && fast.next.next.next!= null){
+        slow = slow.next;
+        fast = fast.next.next;
+     }
+     slow.next = slow.next.next;
+     return head;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public ListNode deleteMiddle(ListNode head) {
+//         if (head == null || head.next == null) {
+//             return null;
+//         }
+//         int size = 0;
+//         ListNode temp = head;
+//         while (temp != null) {
+//             size++;
+//             temp = temp.next;
+//         }
+//         int mid = size / 2;
+//         temp = head;
+//         for (int i = 1; i < mid; i++) {
+//             temp = temp.next;
+//         }
+//         temp.next = temp.next.next;
+//         return head;
+//     }
+// }
