58b7a4bd16f6b30eaa8a19f9cbd7b3052edf0b61
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-09-01 23:35:03 +0530

    Time: 2 ms (91.89%), Space: 47.2 MB (97.87%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,34 @@
-class Solution {
-    public void moveZeroes(int[] arr) {
-        int j = 0; // position for next non-zero element
+// class Solution {
+//     public void moveZeroes(int[] arr) {
+//         int j = 0; // position for next non-zero element
+//         for (int i = 0; i < arr.length; i++) {
+//             if (arr[i] != 0) {
+//                 int temp = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = temp;
+//                 j++;
+//             }
+//         }
+//     }
+// }
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != 0) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                j++;
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+        int[] newArr = new int[nums.length];
+
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] != 0) {
+                newArr[index] = nums[i];
+                index++;
             }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = newArr[i];
         }
     }
 }
