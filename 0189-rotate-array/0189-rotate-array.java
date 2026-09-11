58959fff1f4975417bc1cc9b383aa2e18696ad05
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-09-12 00:10:44 +0530

    Time: 7 ms (7.79%), Space: 273.5 MB (5.18%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,21 +1,54 @@
 class Solution {
-    public static void reverse(int[] arr, int start, int end) {
-        while (start < end) { 
-            //SWAP FIRST TO LAST
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
+      public void rotate(int[] arr, int k)  {
+        int n = arr.length;
+        int[] brr = new int[arr.length];
+        for(int i =0 ; i<arr.length; i++){
+            brr[(i+k)% n] = arr[i];
+        }
+        for(int i = 0; i<n ; i++){
+            arr[i] = brr[i];
         }
     }
-
-    public void rotate(int[] arr, int k) {
-        int n = arr.length;
-        k = k % n; 
-        
-        reverse(arr, 0, n - 1);
-        reverse(arr, 0, k - 1);
-        reverse(arr, k, n - 1);
-    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         while (start < end) { 
+//             //SWAP FIRST TO LAST
+//             int temp = arr[start];
+//             arr[start] = arr[end];
+//             arr[end] = temp;
+//             start++;
+//             end--;
+//         }
+//     }
+
+   // public void rotate(int[] arr, int k) {
+//         int n = arr.length;
+//         k = k % n; 
+//         reverse(arr, 0, n - 1);
+//         reverse(arr, 0, k - 1);
+//         reverse(arr, k, n - 1);
+//     }
+// }
