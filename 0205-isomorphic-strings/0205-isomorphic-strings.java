43ad350c593ef1383fb22a122ba2428ae7a9569f
ayuush2006 <ayushtiwari22nov2003@gmail.com>
2026-09-13 04:19:10 +0530

    Time: 5 ms (98.67%), Space: 43.8 MB (58.55%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,25 +1,53 @@
+// class Solution {
+//     public boolean isIsomorphic(String s, String t) {
+//         char[] a = new char[128];
+//         for (int i = 0; i<s.length() ; i++){
+//             char ch = s.charAt(i);
+//             char dh = t.charAt(i);
+//             int idx = (int)ch;
+//             if(a[idx]=='\0') a[idx]= dh;
+//             else{
+//                 if (a[idx]!= dh) return false ;
+//             }
+//         }
+//         char[] b = new char[128];
+//         for (int i = 0; i<t.length() ; i++){
+//             char ch = t.charAt(i);
+//             char dh = s.charAt(i);
+//             int idx = (int)ch;
+//             if(b[idx]=='\0') b[idx]= dh;
+//             else{
+//                 if (b[idx]!= dh) return false ;    
+//         }
+//     }
+// return true ;
+//     }
+// }
+
+
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        char[] a = new char[128];
-        for (int i = 0; i<s.length() ; i++){
-            char ch = s.charAt(i);
-            char dh = t.charAt(i);
-            int idx = (int)ch;
-            if(a[idx]=='\0') a[idx]= dh;
-            else{
-                if (a[idx]!= dh) return false ;
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] sMap = new int[256];
+        int[] tMap = new int[256];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+
+            if (sMap[a] != tMap[b]) {
+                return false;
             }
+
+            sMap[a] = i + 1;
+            tMap[b] = i + 1;
         }
-        char[] b = new char[128];
-        for (int i = 0; i<t.length() ; i++){
-            char ch = t.charAt(i);
-            char dh = s.charAt(i);
-            int idx = (int)ch;
-            if(b[idx]=='\0') b[idx]= dh;
-            else{
-                if (b[idx]!= dh) return false ;    
-        }
-    }
-return true ;
+
+        return true;
     }
 }
